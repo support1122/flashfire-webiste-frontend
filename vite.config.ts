f98b9e393512c12286a746abfd314d33f56089fe
Pranjal Tripathi <tripathipranjal01@gmail.com>
2025-07-09 00:55:50 +0530

    Fix as before

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ move it here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
