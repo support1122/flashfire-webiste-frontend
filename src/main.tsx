53a6814cca9598dbd1588bab1eea6ba3295528e1
support1122 <support@flashfirejobs.com>
2025-07-14 15:42:51 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import Router from './components/Router.tsx';
+// import './index.css';
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <Router />
+//   </StrictMode>
+// );
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Router from './components/Router.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/Router';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </StrictMode>
 );
+
