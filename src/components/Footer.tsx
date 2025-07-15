a40488f486a5d72d231449b4a99606dca002dd14
Biswajit Sharma <biswajit@Biswajit.>
2025-07-15 14:18:28 +0000

    fix for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
                 REFUND POLICY
               </a>
               <span className="text-orange-200">|</span>
-              <Link to={'/paymentpolicy'} target ='_blank'>
+              <Link to={'flashfirejobs.com/paymentpolicy'} target ='_blank' rel="noopener noreferrer">
               <button
                 // onClick={() => handleLinkClick('/paymentpolicy')}
                 // href='/paymentpolicy'
